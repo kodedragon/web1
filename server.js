7cc29cad86b8eec068c4c46522c243949bbb6c37
kodedragon <knapdragon+github@gmail.com>
2018-06-29 17:01:30 -0400

    Start up script and heroku port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -54,6 +54,6 @@ app.get ('/about', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Server is now running ... ")
+app.listen(port, () => {
+    console.log(`Server is now running on port ${port}... `);
 });
